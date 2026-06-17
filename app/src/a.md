6135fc34c403d1ba2ae25d95b1e2f12d4164d435
ashu <ashu@192.168.1.7>
2026-06-17 21:28:50 +0530

    chore: initial commit

---
+++ app/src/a.md
@@ -0,0 +1,23 @@
+Alright... 
+
+this app is basically a shazam style discovwery plus apple music style sing along built in...
+
+I want the app to include the image from our readme as a mascot where apt.. black and white.. no colors, accept maybe few orange accents here and there...
+
+mobile first UI
+
+start with a add song... spotify link for now.. maybe a browse thing later, option to discover local music, and that should then take them to paste lyruics anmd description for the song (in local scenario..) with AI button
+
+and back home... now the main stuff.. 
+
+a big button to hum along.. 
+
+ when user sings.. we want the bytes available in a placeholder handler..i'll take care of the logic.. idea is we recognise the song and timestamp and play from there with a karaoke UI
+
+go crazy with custom tailwind... also make sure the components live under a package called that under app.. and the rest of the utilities with similar modules too.. 
+
+create sister crates for stuff.. (placeholder)... e.g. search, library, etc
+
+cool
+
+PROCEED...
---
+++ app/src/main.rs
@@ -0,0 +1,65 @@
+use dioxus::prelude::*;
+
+#[derive(Debug, Clone, Routable, PartialEq)]
+#[rustfmt::skip]
+enum Route {
+    #[route("/")]
+    Home {},
+}
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        Echo {}
+    }
+}
+
+/// Echo component that demonstrates fullstack server functions.
+#[component]
+fn Echo() -> Element {
+    let mut response = use_signal(|| String::new());
+
+    rsx! {
+        div {
+            id: "echo",
+            h4 { "ServerFn Echo" }
+            input {
+                placeholder: "Type here to echo...",
+                oninput:  move |event| async move {
+                    let data = echo_server(event.value()).await.unwrap();
+                    response.set(data);
+                },
+            }
+
+            if !response().is_empty() {
+                p {
+                    "Server echoed: "
+                    i { "{response}" }
+                }
+            }
+        }
+    }
+}
+
+/// Echo the user input on the server.
+#[post("/api/echo")]
+async fn echo_server(input: String) -> Result<String, ServerFnError> {
+    Ok(input)
+}
